1283f72322f09f34b2dac6d7ba041dff9f8fcf50
Xenon Zenon <jayson_abad@outlook.com>
2023-03-20 17:38:09 +0800

    0.0.1

--- 2DataTypes.py
+++ 2DataTypes.py
@@ -1,11 +1,59 @@
 # integer
 number = 2
+print("integer: %s" % number)
 
 # float
 numfloat = 2.0
+print("float: %s" % numfloat)
+
+# complex number
+i = 1j
+print("complex number: %s" % i)
 
 # string
 message = "Hello"
+print("string: %s" % message)
 
 # array list
 position = [1, 2]
+print("array list: %s" % position)
+
+# tuple
+computer = ("apple", "ibm")
+print(computer)
+
+# dictionary
+member = {
+    'name' : 'Jayson',
+    'age' : '26'
+}
+print(member)
+
+# set
+cars = {"Ferrari", "Chevrolet", "Toyota"}
+print(cars)
+
+# frozen set
+cars = frozenset({"Ferrari", "Chevrolet", "Toyota"})
+print(cars)
+
+# boolean
+open = True
+print(open)
+
+# bytes
+message = b"Hello"
+print(message)
+
+# byte array
+bytearry = bytearray(5)
+print(bytearry)
+
+# memory view
+memview = memoryview(bytes(5))
+print(memview)
+
+# None
+
+m = None
+print(m)
