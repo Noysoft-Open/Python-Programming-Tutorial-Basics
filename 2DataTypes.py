22059811dfe95b213c73f0ba97672df7872f8669
Xenon Zenon <jayson_abad@outlook.com>
2023-03-20 17:47:32 +0800

    0.0.1

--- 2DataTypes.py
+++ 2DataTypes.py
@@ -1,6 +1,12 @@
 # integer
 number = 2
 print("integer: %s" % number)
+number = -1
+print("integer: %s" % number)
+number = 12E4
+print("integer: %s" % number)
+number = -87.7e100
+print("integer: %s" % number)
 
 # float
 numfloat = 2.0
@@ -9,6 +15,18 @@ print("float: %s" % numfloat)
 # complex number
 i = 1j
 print("complex number: %s" % i)
+i = 3 + 1j
+print("complex number: %s" % i)
+
+# range
+num = range(0, 5)
+print("Range: %s" % num)
+
+# ramdom
+# import is a pre defined keyword that let the interpreter include the random package
+import random
+rand_num = random.randrange(1, 10)
+print("Random: %s" % rand_num)
 
 # string
 message = "Hello"
@@ -54,6 +72,5 @@ memview = memoryview(bytes(5))
 print(memview)
 
 # None
-
 m = None
 print(m)
