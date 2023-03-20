e92a36523ee205ec7e3c4f9767da9f73e2f17773
Xenon Zenon <jayson_abad@outlook.com>
2023-03-20 17:20:26 +0800

    0.0.1

--- 1HelloWorld.py
+++ 1HelloWorld.py
@@ -1,1 +1,11 @@
+# name is a varialble that contains string value
+# a string is data type that contains characters
+name = "Xenon Zenon"
+
+# print() is a python function that send data that it contains inside its parenthesis to the console
+# the varialble name is a data that contains string that is declared before the function
+print(name)
 print("Hello World")
+
+# a function such as print() can contain string, varialble, integer, float, array list
+print(2)
--- 2DataTypes.py
+++ 2DataTypes.py
@@ -1,3 +1,11 @@
+# integer
 number = 2
-string = "Hello"
-array = [1, 2]
+
+# float
+numfloat = 2.0
+
+# string
+message = "Hello"
+
+# array list
+position = [1, 2]
